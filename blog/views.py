87f63ee0781d608a6ab7a2337ba50d8e54d4cc12
mdillar <marc.dillar@gmail.com>
2021-06-20 00:58:21 +0200

    Replace the posts placeholder by the actual posts lists from the DB

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-{"author": "MarcD", "title": "Blog Post 1", "content":"First Post Content", "date_posted":"August 27, 2018"},
-{"author": "Jane Doe", "title": "Blog Post 2", "content":"Second Post Content", "date_posted":"August 28, 2018"}
-
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, "blog/home.html", context)
 
