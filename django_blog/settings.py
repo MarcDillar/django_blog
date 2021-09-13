85ebc98951e957ecad6a5cd8d963a3136c84c07f
Marc Dillar <marc.dillar@gmail.com>
2021-09-13 09:19:35 +0200

    set debug value as env variable

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_BLOG_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.getenv("DEBUG")=="True")
 
 ALLOWED_HOSTS = [
     'mdillar-django-blog.herokuapp.com'
